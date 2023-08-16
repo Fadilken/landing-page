2d27a5f40e506bac08c2d68236bc250bf744e059
Fadilken <kenztzy2121@gmail.com>
2023-08-16 09:26:04 +0800

    change scroll reveal

---
+++ script.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    distance: "80px",
+    delay: 200,
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.fromRight', {origin: "right"});
+ScrollReveal().reveal('.fromLeft', {origin: "left"});
+ScrollReveal().reveal('.fromBottom', {origin: "bottom"});
